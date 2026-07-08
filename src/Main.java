f7006619d0a90a8ba8f2c3cedaba0b67c56db468
DiogoSeveroAntunes <tchais777@hotmail.com>
2026-07-08 02:21:22 -0300

    Trocando para lista

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-    int num = 10;
-    System.out.println(num);
-    Integer numWrapper = Integer.valueOf(num);
-        System.out.println(numWrapper);
+        exemploListaSimples();
+        exemploListaOrdenada();
+    }
+    private static void exemploListaSimples(){
+        Scanner s = new Scanner(System.in);
+        List<String> lista = new ArrayList<>();
+
+        System.out.println("Digite o nome das pessoa desta lista, separando cada um com (,):");
+        String resposta = s.nextLine();
+        lista.addAll(Arrays.asList(resposta.split(",")));
+        System.out.println(lista);
+        System.out.println(lista);
+    }
+    private static void exemploListaOrdenada(){
+    Scanner s = new Scanner(System.in);
+    List<String> lista = new ArrayList<>();
+
+    System.out.println("Digite o nome das pessoa desta lista com ordenação, separando cada um com (,):");
+    String resposta = s.nextLine();
+    lista.addAll(Arrays.asList(resposta.split(",")));
+    Collections.sort(lista);
+
+    System.out.println(lista);
+    System.out.println("");
+
 
     }
 }
