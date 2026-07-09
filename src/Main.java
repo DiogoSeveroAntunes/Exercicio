9e78c7baf317aba0290d707e3783c003d02647be
DiogoSeveroAntunes <tchais777@hotmail.com>
2026-07-08 22:23:39 -0300

    Fazendo lista com Map e com List normal.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,50 @@ public class Main {
     public static void main(String[] args) {
         exemploListaSimples();
         exemploListaOrdenada();
+        exemploListaComValoresSimples();
+        exemploListaComValoresOrdenados();
+    }
+    private static void exemploListaComValoresOrdenados() {
+        Map<String, String> lista = new HashMap<>();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Escreva o nome e o sexo, se parando os nomes utilizando (,) e o sexo desse modo (nome)+(-) e o sexo(M ou F)");
+        String resultado = s.nextLine();
+        String[] split = resultado.split(",");
+        for (String pessoa : split) {
+            String[] dados = pessoa.split("-");
+            if (dados.length == 2) {
+                String nomes = dados[0];
+                String sexo = dados[1];
+                lista.put(nomes, sexo);
+            }
+            Map<String, String> listaOrdenada = new TreeMap<>(lista);
+            System.out.println(listaOrdenada);
+        }
+    }
+    private static void exemploListaComValoresSimples() {
+        Map<String, String> lista = new HashMap<>();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Escreva o nome e o sexo, se parando os nomes utilizando (,) e o sexo desse modo (nome)+(-) e o sexo(M ou F)");
+        String resultado = s.nextLine();
+        String[] split = resultado.split(",");
+        for (String pessoa : split) {
+            String[] dados = pessoa.split("-");
+            if (dados.length == 2) {
+                String nomes = dados[0];
+                String sexo = dados[1];
+                lista.put(nomes, sexo);
+            }
+            System.out.println(lista);
+        }
+    }
+    private static void exemploListaOrdenada(){
+        Scanner s = new Scanner(System.in);
+        List<String> lista = new ArrayList<>();
+        System.out.println("Digite o nome das pessoa desta lista com ordenação, separando cada um com (,):");
+        String resposta = s.nextLine();
+        lista.addAll(Arrays.asList(resposta.split(",")));
+        Collections.sort(lista);
+        System.out.println(lista);
     }
     private static void exemploListaSimples(){
         Scanner s = new Scanner(System.in);
@@ -14,15 +58,5 @@ public class Main {
         lista.addAll(Arrays.asList(resposta.split(",")));
         System.out.println(lista);
         System.out.println(lista);
-    }
-    private static void exemploListaOrdenada(){
-    Scanner s = new Scanner(System.in);
-    List<String> lista = new ArrayList<>();
-
-    System.out.println("Digite o nome das pessoa desta lista com ordenação, separando cada um com (,):");
-    String resposta = s.nextLine();
-    lista.addAll(Arrays.asList(resposta.split(",")));
-    Collections.sort(lista);
-    System.out.println(lista);
     }
 }
